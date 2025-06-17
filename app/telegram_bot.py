cd135845efcedf07f69d4fcbfcf3c83cd8e264de
salavatgibadullin <salavat9154@gmail.com>
2025-06-17 18:13:01 +0500

    рабочий вариант

--- app/telegram_bot.py
+++ app/telegram_bot.py
@@ -8,18 +8,7 @@ load_dotenv()
 
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-#
-# def notify_city_masters(city_id, request_data):
-#     db = SessionLocal()
-#     masters = db.query(Master).filter(Master.city_id == city_id).all()
-#     text = f"Новая заявка:\nИмя: {request_data.name}\nТел: {request_data.phone}\nПроблема: {request_data.description}"
-#
-#     for master in masters:
-#         requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", params={
-#             # "chat_id": master.telegram_id,
-#             "chat_id": CHAT_ID,
-#             "text": text
-#         })
+
 
 
 def notify_city_masters(city_id, requests_data):
@@ -35,7 +24,7 @@ def notify_city_masters(city_id, requests_data):
         # f'\nИмя: {requests_data.name}'
             f'\n📍 Город: {requests_data.city.name}'
             f'\n📱 Телефон: {requests_data.phone}'
-            f'\n📱 Модель / Неисправность: {requests_data.description}')
+            f'\n🗒 ️Модель / Неисправность: {requests_data.description}')
 
     for master in masters:
         chat_id = master.telegram_id
@@ -46,25 +35,6 @@ def notify_city_masters(city_id, requests_data):
         print(f"Response for {chat_id}: {response.status_code} {response.text}")
 
 
-#
-# def notify_city_masters(city_id, requests_data):
-#     print(f"[DEBUG] Отправка уведомлений по городу ID: {city_id}")
-#     print(f"[DEBUG] Заявка: {requests_data.phone}, {requests_data.description}")
-#
-#     db = SessionLocal()
-#     masters = db.query(Master).filter(Master.city_id == city_id).all()
-#     print(f"[DEBUG] Найдено мастеров: {len(masters)}")
-#
-#     text = (
-#         f'🔧 Новая заявка:\n'
-#         f'📱 Телефон: {requests_data.phone}\n'
-#         f'📝 Описание: {requests_data.description}'
-#     )
-#
-#     for master in masters:
-#         chat_id = master.telegram_id
-#         response = requests.get(
-#             f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-#             params={"chat_id": chat_id, "text": text}
-#         )
-#         print(f"Response for {chat_id}: {response.status_code} {response.text}")
+
+
+
