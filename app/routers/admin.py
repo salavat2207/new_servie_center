4a850db1e55d1206b037f3f4882f471ee33e053d
salavatgibadullin <salavat9154@gmail.com>
2025-06-21 16:22:02 +0500

    почти-рабочий вариант

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -7,7 +7,7 @@ import os
 UPLOAD_DIR = "static/uploads"
 from app import schemas, crud
 from app.database import get_db
-from app.models import Product, ProductPrice, User, Admin
+from app.models import Product, ProductPrice, User, Admin, RepairService, City
 from app.routers.auth import get_current_user
 from app.schemas import ProductCreate, ProductPriceCreate, ProductPriceSchema, AdminLoginSchema, ProductCreateSchema
 from app.auth import verify_password, create_access_token, get_current_admin, get_password_hash, get_current_admin_user
@@ -22,50 +22,94 @@ router = APIRouter()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="admin/login")
 
+#
+# @router.post("/admin/products")
+# def create_product(
+# 		product_data: ProductCreateSchema,
+# 		db: Session = Depends(get_db),
+# 		_: dict = Depends(get_current_admin_user),
+# ):
+# 	if db.query(Product).filter_by(id=product_data.id).first():
+# 		raise HTTPException(status_code=400, detail="Product already exists")
+#
+# 	product = Product(
+# 		id=product_data.id,
+# 		name=product_data.name,
+# 		category=product_data.category,
+# 		title=product_data.title,
+# 		link=product_data.link,
+# 		category_id=product_data.category_id,
+# 		description=product_data.description,
+# 		image=product_data.image
+# 	)
+# 	db.add(product)
+# 	db.commit()
 
-@router.post("/admin/products")
-def create_product(
-		product_data: ProductCreateSchema,
-		db: Session = Depends(get_db),
-		_: dict = Depends(get_current_admin_user),
-):
-	if db.query(Product).filter_by(id=product_data.id).first():
-		raise HTTPException(status_code=400, detail="Product already exists")
-
-	product = Product(
-		id=product_data.id,
-		name=product_data.name,
-		category=product_data.category,
-		title=product_data.title,
-		link=product_data.link,
-		category_id=product_data.category_id,
-		description=product_data.description,
-		image=product_data.image
-	)
-	db.add(product)
-	db.commit()
 
 
-@router.post("/product-price")
-def set_product_price(data: ProductPriceSchema, db: Session = Depends(get_db)):
-	existing = db.query(ProductPrice).filter_by(
-		product_id=data.product_id,
-		city_id=data.city_id
-	).first()
 
-	if existing:
-		existing.price = data.price
-		db.commit()
-		return {"message": "Цена обновлена"}
-	else:
-		new_price = ProductPrice(
-			product_id=data.product_id,
-			city_id=data.city_id,
-			price=data.price
-		)
-		db.add(new_price)
-		db.commit()
-		return {"message": "Новая цена добавлена"}
+
+"""
+Добавление услуг
+"""
+@router.post("/add")
+def add_product(product: ProductPriceCreate, db: Session = Depends(get_db)):
+    existing_product = db.query(RepairService).filter(RepairService.id == product.id).first()
+    if existing_product:
+        raise HTTPException(status_code=400, detail="Товар с таким ID уже существует")
+
+    new_product = RepairService(
+        id=product.id,
+        name=product.name,
+        service_id=product.service_id,
+        price=product.price,
+        city_id=product.city_id,
+        description=product.description,
+        duration=product.duration,
+        product_id=product.product_id,
+        category_id=product.category_id
+    )
+    try:
+        db.add(new_product)
+        db.commit()
+        db.refresh(new_product)
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f"Ошибка при добавлении: {str(e)}")
+
+    return {"message": "Товар успешно добавлен", "product": {
+        "id": new_product.id,
+        "name": new_product.name,
+        'service_id': new_product.service_id,
+        "price": new_product.price,
+        "city_id": new_product.city_id,
+        "description": new_product.description,
+        "duration": new_product.duration,
+        'product_id': new_product.product_id,
+        'category_id': new_product.category_id
+    }}
+
+#
+# @router.post("/product-price")
+# def set_product_price(data: ProductPriceSchema, db: Session = Depends(get_db)):
+# 	existing = db.query(ProductPrice).filter_by(
+# 		product_id=data.product_id,
+# 		city_id=data.city_id
+# 	).first()
+#
+# 	if existing:
+# 		existing.price = data.price
+# 		db.commit()
+# 		return {"message": "Цена обновлена"}
+# 	else:
+# 		new_price = ProductPrice(
+# 			product_id=data.product_id,
+# 			city_id=data.city_id,
+# 			price=data.price
+# 		)
+# 		db.add(new_price)
+# 		db.commit()
+# 		return {"message": "Новая цена добавлена"}
 
 
 """
@@ -239,21 +283,37 @@ def upload_image(file: UploadFile = File(...)):
     return {"url": f"/{file_path}"}
 
 
-# Получение цен по городам для продукта
-@router.get("/admin/product-price/{product_id}", dependencies=[Depends(get_current_admin)])
-def get_prices_by_product(product_id: str, db: Session = Depends(get_db)):
-    prices = db.query(ProductPrice).filter_by(product_id=product_id).all()
-    return [{"city_id": p.city_id, "price": p.price} for p in prices]
+# Получение цен по городам для товара
+# @router.get("/admin/product-price/{product_id}", dependencies=[Depends(get_current_admin)])
+# def get_prices_by_product(product_id: str, db: Session = Depends(get_db)):
+#     prices = db.query(ProductPrice).filter_by(product_id=product_id).all()
+#     return [{"city_id": p.city_id, "price": p.price} for p in prices]
 
+@router.get("/admin/product-price/{service_id}", dependencies=[Depends(get_current_admin)])
+def get_prices_by_product(service_id: str, db: Session = Depends(get_db)):
+    services = db.query(RepairService).filter_by(service_id=service_id).all()
+    print(f"[DEBUG] Найдено услуг: {len(services)}")
 
+    result = []
 
+    for service in services:
+        print(f"[DEBUG] Услуга: {service.service_id}")
+        for price in service.prices:
+            print(f"[DEBUG] Цена: {price.price}, город: {price.city_code}")
+            city = db.query(City).filter(City.code == price.city_code).first()
+            result.append({
+                "service_name": service.name,
+                "city": city.name if city else price.city_code,
+                "price": price.price
+            })
 
-
-router.post("/admin/requests/{request_id}/status", dependencies=[Depends(get_current_admin)])
-def update_request_status(request_id: int, data: dict, db: Session = Depends(get_db)):
-    request = db.query(Request).filter_by(id=request_id).first()
-    if not request:
-        raise HTTPException(status_code=404, detail="Заявка не найдена")
-    request.status = data.get("status")
-    db.commit()
-    return {"message": "Статус обновлён", "status": request.status}
+    return result
+#
+# router.post("/admin/requests/{request_id}/status", dependencies=[Depends(get_current_admin)])
+# def update_request_status(request_id: int, data: dict, db: Session = Depends(get_db)):
+#     request = db.query(Request).filter_by(id=request_id).first()
+#     if not request:
+#         raise HTTPException(status_code=404, detail="Заявка не найдена")
+#     request.status = data.get("status")
+#     db.commit()
+#     return {"message": "Статус обновлён", "status": request.status}
