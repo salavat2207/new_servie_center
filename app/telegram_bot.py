6d215625a1a959a3fe497d64c7353a362e736d3e
salavatgibadullin <salavat9154@gmail.com>
2025-06-24 19:39:36 +0500

    рабочий тг бот

--- app/telegram_bot.py
+++ app/telegram_bot.py
@@ -1,3 +1,4 @@
+import json
 from app.database import SessionLocal
 from app.models import Master, RepairRequest
 import requests
@@ -11,13 +12,17 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 
 
-def notify_city_masters(city_id, requests_data):
-    print(f"[DEBUG] Отправка уведомлений по городу ID: {city_id}")
-    print(f"[DEBUG] Заявка: {requests_data.phone}, {requests_data.description}")
+from fastapi import FastAPI, Request
 
+app = FastAPI()
+
+
+
+
+
+def notify_city_masters(city_id, requests_data):
     db = SessionLocal()
     masters = db.query(Master).filter(Master.city_id == city_id).all()
-    print(f"[DEBUG] Найдено мастеров: {len(masters)}")
 
     text = (
         f'🛠 Заявка на консультацию:'
@@ -38,3 +43,25 @@ def notify_city_masters(city_id, requests_data):
 
 
 
+
+
+
+
+# def notify_city_masters(city_id, requests_data):
+#     db = SessionLocal()
+#     masters = db.query(Master).filter(Master.city_id == city_id).all()
+#
+#     text = (
+#         f'🛠 Заявка на консультацию:'
+#         # f'\nИмя: {requests_data.name}'
+#             f'\n📍 Город: {requests_data.city.name}'
+#             f'\n📱 Телефон: {requests_data.phone}'
+#             f'\n🗒 ️Модель / Неисправность: {requests_data.description}')
+#
+#     for master in masters:
+#         chat_id = master.telegram_id
+#         response = requests.get(
+#             f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+#             params={"chat_id": chat_id, "text": text}
+#         )
+#         print(f"Response for {chat_id}: {response.status_code} {response.text}")
